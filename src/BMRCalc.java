a5028f9d0ce10a2a7a9615684f8b53e959c45b21
CptAhab1 <habbart01@gmail.com>
2024-02-23 16:12:04 -0600

    Comments and final edits

--- src/BMRCalc.java
+++ src/BMRCalc.java
@@ -1,18 +1,24 @@
+// This class will handle calculations for calories needed per day to maintain weight
 public class BMRCalc {
 
-
+    // This function will calculate the calories per day needed for a biological female to maintain weight,
+    // before accounting for activity level.
     public double calcBMRFemale(int weight, int height, int age) {
 
         return (655 + (4.3*weight) + 4.7*(height-age));
 
     }
-
+    // This function will calculate the calories per day needed for a biological male to maintain weight,
+    // before accounting for activity level.
     public double calcBMRMale(int weight, int height, int age){
 
         return (66 + (6.3*weight) + (12.9*height) - (6.8*age));
 
     }
 
+    // This function will determine the factor we must multiply the calories per day by
+    // depending on user-input for activity level
+    // This includes input verification for anything besides the 4 accepted levels
     public double actModCalc(char activity){
         double actMod = 0;
         switch(activity) {
--- src/CaloriesCounter.java
+++ src/CaloriesCounter.java
@@ -1,15 +1,32 @@
+/*
+Alex Habbart
+0855541
+2/23/24
+CSC 512
+Lab 3
+ */
+
+// This program will calculate how many candy bars of a given calorie value
+// one must eat per day to maintain weight.
+
+
 import java.util.Scanner;
 
 public class CaloriesCounter {
 
     public static void main(String[] args) {
 
+        // Intro message
         System.out.println("This program will calculate the number of 230 calorie\n" +
                 "candy bars to eat to maintain your weight. \n");
 
+        // Declaring this variable here will allow us to change the calories per candy bars later if needed
         double barCal = 230;
 
+        // Scanner object
         Scanner scanner = new Scanner(System.in);
+
+        // The next 3 blocks will read user input for age, height, and weight.
 
         System.out.print("\t [Q1] What is your age in years? ");
         int age = scanner.nextInt();
@@ -20,13 +37,17 @@ public class CaloriesCounter {
         System.out.print("\t [Q3] What is your weight in pounds? ");
         int weight = scanner.nextInt();
 
+        // Since there's no nextChar function, we read sex as a string
+        // then take the first char from that single-letter string
         System.out.print("\t [Q4] Enter 'M'(or 'm') for male or 'F'(or 'f') for female: ");
         String sexString = scanner.next();
         char sex = Character.toLowerCase(sexString.charAt(0));
         System.out.println();
 
+        // declaring object for BMRCalc class
         BMRCalc calculation = new BMRCalc();
 
+        // This variable tells how many calories one needs to maintain before factoring activity level
         double calToMaintain;
 
         switch (sex) {
@@ -37,11 +58,14 @@ public class CaloriesCounter {
                 calToMaintain = calculation.calcBMRFemale(weight, height, age);
                 break;
             default:
+                // If the user inputs a sex besides m or f, program will end early
                 System.out.println("This program does not have functionality for intersex people :(");
                 System.out.println("Program ended.");
                 System.exit(0);
         }
 
+        // Similar to the sex variable, for activity level we take the first char of a string
+        // entered by the user
         System.out.print("\t [Q5] Are you: \n");
         System.out.println("\t \t A. Sedentary");
         System.out.println("\t \t B. Somewhat active (exercise occasionally)");
@@ -50,8 +74,13 @@ public class CaloriesCounter {
         System.out.print("\t \t Enter A, B, C, or D: ");
         String activityString = scanner.next();
         char activity = Character.toLowerCase(activityString.charAt(0));
+
+        // This variable will be calculated in the BMRCalc class and be multiplied with the calorie total
+        // to tell how many calories one must eat in a day to maintain weight
         double actMod = calculation.actModCalc(activity);
 
+        // We must divide the calories needed to maintain weight per day by the calories in a candy bar
+        // to determine the candy bars per day needed to maintain weight.
         double barsToMaintain = 0;
 
         switch (sex) {
@@ -63,9 +92,13 @@ public class CaloriesCounter {
                 break;
         }
 
-        System.out.println("With those measurements, you should eat " +
+        // This will print the result
+        System.out.println("\nWith those measurements, you should eat " +
                 barsToMaintain +
                 " candy bars per day to maintain the same weight.");
+
+        // Exit message
+        System.out.println("\nProgram ended.");
 
     }
 }
